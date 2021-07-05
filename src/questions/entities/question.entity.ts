521321476c394180a93c034c7078bbd5b699b8bc
Fausto Schwantz <faustoschwantz@gmail.com>
2021-07-04 22:57:51 -0300

    chore: added  entity questions

--- src/questions/entities/question.entity.ts
+++ src/questions/entities/question.entity.ts
@@ -1,4 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('questions')
 export class Question {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   statement: string;
 }
