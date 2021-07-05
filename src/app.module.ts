23eee3000adf5559622a14fc27fba99b9db274f7
Fausto Schwantz <faustoschwantz@gmail.com>
2021-07-04 21:11:54 -0300

    chore: added .env and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,29 @@ import { AppService } from './app.service';
 import { ExamsModule } from './exams/exams.module';
 import { QuestionsModule } from './questions/questions.module';
 import { OptionsModule } from './options/options.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ExamsModule, QuestionsModule, OptionsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [process.env.TYPEORM_ENTITIES],
+      migrations: [process.env.TYPEORM_MIGRATIONS],
+      cli: {
+        migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+      },
+    }),
+    ExamsModule,
+    QuestionsModule,
+    OptionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
