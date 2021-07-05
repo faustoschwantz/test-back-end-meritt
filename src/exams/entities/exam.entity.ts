d4228add70cbf1e6caed067ef3689d54631cfe63
Fausto Schwantz <faustoschwantz@gmail.com>
2021-07-04 23:05:45 -0300

    chore: added entity exams

--- src/exams/entities/exam.entity.ts
+++ src/exams/entities/exam.entity.ts
@@ -1,8 +1,17 @@
 import { ExamTypes } from 'src/shared/types/exam';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('exams')
 export class Exam {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column('')
   name: string;
+
+  @Column('')
   description: string;
+
+  @Column('')
   type: ExamTypes;
 }
