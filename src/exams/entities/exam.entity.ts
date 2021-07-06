cd848a2328bfbb0578b5248ef83e757666383a8a
Fausto Schwantz <faustoschwantz@gmail.com>
2021-07-05 21:52:35 -0300

    feat: added exams/questions relations

--- src/exams/entities/exam.entity.ts
+++ src/exams/entities/exam.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Question } from './../../questions/entities/question.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('exams')
 export class Exam {
@@ -13,4 +14,7 @@ export class Exam {
 
   @Column()
   type: string;
+
+  @OneToMany(() => Question, (question) => question.exam)
+  questions: Question[];
 }
--- src/questions/entities/question.entity.ts
+++ src/questions/entities/question.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Exam } from 'src/exams/entities/exam.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('questions')
 export class Question {
@@ -7,4 +8,10 @@ export class Question {
 
   @Column()
   statement: string;
+
+  @Column()
+  examId: string;
+
+  @ManyToOne(() => Exam, (exam) => exam.questions)
+  exam: Exam;
 }
