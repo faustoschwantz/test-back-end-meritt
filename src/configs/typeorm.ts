f2cf0c2c065f7053d2e4768464d8e55c78012cfb
Fausto Schwantz <faustoschwantz@gmail.com>
2021-07-10 22:30:30 -0300

    chore: typeorm test

--- src/configs/typeorm.ts
+++ src/configs/typeorm.ts
@@ -13,6 +13,7 @@ export const typeormConfig = (): TypeOrmModuleOptions => {
   if (process.env.DATABASE_URL)
     return {
       ...base,
+      ssl: true,
       url: process.env.DATABASE_URL,
     };
 
