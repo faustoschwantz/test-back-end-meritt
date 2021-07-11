236e341cbb013e4079ff8566541cc48e76bc98d3
Fausto Schwantz <faustoschwantz@gmail.com>
2021-07-10 22:08:59 -0300

    chore: typeorm env config

--- src/configs/typeorm.ts
+++ src/configs/typeorm.ts
@@ -1,17 +1,27 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeormConfig = (): TypeOrmModuleOptions => {
-  return {
+  const base = {
     type: process.env.TYPEORM_CONNECTION as any,
-    host: process.env.TYPEORM_HOST,
-    port: parseInt(process.env.TYPEORM_PORT),
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE,
     autoLoadEntities: true,
     migrations: [process.env.TYPEORM_MIGRATIONS_DIST],
     cli: {
       migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
     },
+  };
+
+  if (process.env.DATABASE_URL)
+    return {
+      ...base,
+      url: process.env.DATABASE_URL,
+    };
+
+  return {
+    ...base,
+    host: process.env.TYPEORM_HOST,
+    port: parseInt(process.env.TYPEORM_PORT),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
   };
 };
